d183ef89ba8a0dffdadf4b120c7c858b449afe31
Sebas Hollow <Sebas.V.Hollow@gmail.com>
2015-11-24 01:19:22 +0200

    Added hardcoded moves

--- src/Main.hs
+++ src/Main.hs
@@ -2,46 +2,93 @@
 module Main where
 
 import Parser
-import Board (getTurn, Square)
+import Board
 import Composer (serialize)
 import Network.HTTP.Client
 import Network.HTTP.Types.Status (statusCode)
-import Data.String (fromString)
+import Data.String
 import Text.Printf (printf)
-
-testMsg = "l[m[\"x\"; 2; \"y\"; 1; \"v\"; \"x\"]; m[\"x\"; 2; \"y\"; 0; \"v\"; \"o\"]; m[\"x\"; 0; \"y\"; 1; \"v\"; \"x\"]; m[\"x\"; 0; \"y\"; 0; \"v\"; \"o\"]; m[\"x\"; 1; \"y\"; 2; \"v\"; \"x\"]; m[\"x\"; 1; \"y\"; 1; \"v\"; \"o\"]; m[\"x\"; 1; \"y\"; 0; \"v\"; \"x\"]; m[\"x\"; 0; \"y\"; 2; \"v\"; \"o\"]]"
-testMsg' = "l[m[\"x\"; 2; \"y\"; 1; \"v\"; \"x\"]; m[\"x\"; 2; \"y\"; 0; \"v\"; \"o\"]; m[\"x\"; 0; \"y\"; 1; \"v\"; \"x\"]; m[\"x\"; 0; \"y\"; 0; \"v\"; \"o\"]; m[\"x\"; 1; \"y\"; 2; \"v\"; \"x\"]; m[\"x\"; 1; \"y\"; 1; \"v\"; \"o\"]; m[\"x\"; 1; \"y\"; 0; \"v\"; \"x\"]; m[\"x\"; 0; \"y\"; 2; \"v\"; \"o\"]]"
+import Data.ByteString.Lazy (toStrict)
+import Data.ByteString.Char8 (unpack)
 
 format :: String
-gameID :: String
 playerID :: Int
+turns :: [Square]
 
+encoding = "application/m-expr+list"
 format = "http://tictactoe.homedir.eu/game/%s/player/%d"
-gameID = "test-1231234-51525-51"
-playerID = 1
+--gameID = "test-1252346sdfnsfjw7ssasz7"
+playerID = 2
+turns = [(0, 2, 'o'), (2, 2, 'o')]
 
 main :: IO ()
-main = postData
+main = do
+    putStrLn "Enter unique game ID: "
+    gameID <- getLine
+    let url = printf format gameID playerID
 
-requestBody' = RequestBodyBS $ fromString $ serialize testMsg'
-
-postData = do
     manager <- newManager defaultManagerSettings
 
-    initialRequest <- parseUrl $ printf format gameID (1 :: Int)
-    let request = initialRequest { method = "POST", requestBody = requestBody', requestHeaders = [("Content-Type", "application/m-expr+list")] }
+    board <- getData manager url
+
+    postData manager url $ board ++ [head turns]
+    board <- getData manager url
+
+    postData manager url $ board ++ [last turns]
+    board <- getData manager url
+
+    print "Game finished. You lost!"
+
+postData :: Manager -> String -> [Square] -> IO ()
+postData manager url board = do
+    initialRequest <- parseUrl url
+    let request = initialRequest { method = "POST"
+                                 , requestBody = (toRequestBody board)
+                                 , requestHeaders = [("Content-Type", encoding)]}
 
     response <- httpLbs request manager
-    putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
-    print $ responseBody response
+    --putStrLn $ "Status code: " ++ (show . statusCode $ responseStatus response)
+    putStrLn $ "Sent:    " ++ show board
 
-getData = do
-    manager <- newManager defaultManagerSettings
-
-    request <- parseUrl $ printf format gameID (2 :: Int)
-    let request' = request { method = "GET", requestHeaders = [("Accept", "application/m-expr+list")] }
+getData :: Manager -> String -> IO [Square]
+getData manager url = do
+    request <- parseUrl url
+    let request' = request { method = "GET"
+                           , requestHeaders = [("Accept", encoding)] }
     response <- httpLbs request' manager
 
-    putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
-    print $ responseBody response
+    --putStrLn $ "Status code: " ++ (show . statusCode $ responseStatus response)
+    let board = parse $ unpack $ toStrict (responseBody response)
+    putStrLn $ "Received " ++ show board
+    return board
 
+toRequestBody :: [Square] -> RequestBody
+toRequestBody string = RequestBodyBS . fromString $ serialize string
+
+
+{-httpTest :: IO ()
+httpTest = do
+    putStrLn "Enter unique game ID: "
+    gameID <- getLine
+    let url = printf format gameID (1 :: Int)
+    let url2 = printf format gameID (2 :: Int)
+
+    manager <- newManager defaultManagerSettings
+    manager2 <- newManager defaultManagerSettings
+
+    postData manager2 url2 [(0, 1, 'x')]
+
+    board <- getData manager url
+    postData manager url $ board ++ [head turns]
+
+    board <- getData manager2 url2
+    postData manager2 url2 $ board ++ [(1, 1, 'x')]
+
+    board <- getData manager url
+    postData manager url $ board ++ [last turns]
+
+    board <- getData manager2 url2
+    postData manager2 url2 $ board ++ [(2, 1, 'x')]
+
+    board <- getData manager url
+    print "Game finished. You lost!"-}
